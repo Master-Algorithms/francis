d5068f925b4a799523de90dda9e56b052e67b64d
Francis <bup9812@naver.com>
2020-10-01 13:53:13 +0900

    greedy

--- cote_book/11-1. 모험가길드.py
+++ cote_book/11-1. 모험가길드.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 n = int(input())
 list = [int(i) for i in input().split()]
 print(list)
@@ -13,20 +12,4 @@ for i in list:
         group += 1
         cur = 0
 
-=======
-n = int(input())
-list = [int(i) for i in input().split()]
-print(list)
-list = sorted(list)
-print(list)
-group = 0
-cur = 0
-
-for i in list:
-    cur += 1
-    if cur >= i:
-        group += 1
-        cur = 0
-
->>>>>>> 033258224181217757944f3e21cf4c9dbb5db85e
 print(group)
